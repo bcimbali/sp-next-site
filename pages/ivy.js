894ad6d90db6816299e39d210bb4417a975c6255
Brett Cimbalik <bcimbali@gmail.com>
2020-08-22 15:37:50 -0500

    add ivy page

---
+++ pages/ivy.js
@@ -0,0 +1,9 @@
+import Layout from './../components/layout';
+
+export default function Ivy() {
+  return (
+    <Layout logoColor="violet" videoName="ivy-no-audio" nextPage="">
+      
+    </Layout>
+  )
+}
