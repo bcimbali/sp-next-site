bec2ce48eba5d2708bace9462bea1b47a289e11a
Brett Cimbalik <bcimbali@gmail.com>
2020-08-22 21:01:10 -0500

    add trailers video and fill animation

--- pages/ivy.js
+++ pages/ivy.js
@@ -2,7 +2,7 @@ import Layout from './../components/layout';
 
 export default function Ivy() {
   return (
-    <Layout logoColor="violet" videoName="ivy-no-audio" nextPage="">
+    <Layout logoColor="violet" videoName="ivy-no-audio" nextPage="trailers">
       
     </Layout>
   )
